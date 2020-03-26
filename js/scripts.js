3269330cd6ef87b73943dc76b23e46de9fdb2cb5
vovatyshchenko <vovakramaha@gmail.com>
2020-03-26 12:22:12 +0200

    some refactoring

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,7 @@
     "use strict";
 
     // Add active state to sidbar nav links
-    var path = window.location.href; // because the 'href' property of the DOM element is the absolute path
+    let path = window.location.href; // because the 'href' property of the DOM element is the absolute path
         $("#layoutSidenav_nav .sb-sidenav a.nav-link").each(function() {
             if (this.href === path) {
                 $(this).addClass("active");
@@ -19,4 +19,8 @@
         e.preventDefault();
         $("body").toggleClass("sb-sidenav-toggled");
     });
-})(jQuery);
+});
+  $(document).ready(function() {
+  $('#dataTable').DataTable();
+});
+

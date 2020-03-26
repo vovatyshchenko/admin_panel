295593f2c9689dc2fc5e80c3e04e86bbc53946b5
vovatyshchenko <vovakramaha@gmail.com>
2020-03-26 17:31:33 +0200

    add some styles

--- js/scripts.js
+++ js/scripts.js
@@ -23,4 +23,16 @@
   $(document).ready(function() {
   $('#dataTable').DataTable();
 });
+  $(function() {
+      $('input[name="birthday"]').daterangepicker({
+        singleDatePicker: true,
+        showDropdowns: true,
+        minYear: 1901,
+        maxYear: parseInt(moment().format('YYYY'),10)
+      })
+    //function(start, end, label) {
+    //let years = moment().diff(start, 'years');
+    //alert("You are " + years + " years old!");
+  //});
+});
 
